4e00f4e13f38993c4cbb881b36b03c59304d466d
hr <ipowerink@outlook.com>
2017-08-17 16:26:56 +0800

    显示本机类型大小

---
+++ Cplus复习/review01.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+// 显示本机各个类型的大小
+void showTypeSize() {
+	cout << "Size of char : " << sizeof(char) << endl;
+	cout << "Size of int : " << sizeof(int) << endl;
+	cout << "Size of short int : " << sizeof(short int) << endl;
+	cout << "Size of long int : " << sizeof(long int) << endl;
+	cout << "Size of float : " << sizeof(float) << endl;
+	cout << "Size of double : " << sizeof(double) << endl;
+	cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+}
+
+int main() {
+	showTypeSize();
+	return 0;
+}
